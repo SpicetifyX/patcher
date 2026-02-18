c6a198e12ac72ea0c75469800c32ce980514eb2c
Jeffrey Reij <contact@jreij.info>
2026-02-18 17:12:12 +0100

    Update spotify.go

--- internal/spotify/spotify.go
+++ internal/spotify/spotify.go
@@ -1,3 +1,3 @@
 package spotify
 
-const DIRECT_DOWNLOAD_URL = "https://download.scdn.co/SpotifyFullSetup.exe"
+const DIRECT_DOWNLOAD_URL = "https://upgrade.scdn.co/upgrade/client/win32-x86_64/spotify_installer-1.2.83.461.g4d9c3dae-801.exe"
