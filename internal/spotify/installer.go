04a5abd59aecf68c1c406915814b6a788c598e51
Jeffrey Reij <contact@jreij.info>
2026-02-18 06:08:11 +0100

    Update installer.go

--- internal/spotify/installer.go
+++ internal/spotify/installer.go
@@ -1,1 +1,65 @@
 package spotify
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+)
+
+func InstallStandaloneSpotfiy(outDir string, overwriteExisting bool) {
+	if _, err := os.Stat(outDir); err == nil {
+		log.Println("Spotify installation already exists in this folder")
+		if overwriteExisting {
+			err = os.RemoveAll(outDir)
+			if err != nil {
+				log.Panicln(err)
+			}
+		} else {
+			return
+		}
+	}
+
+	err := os.MkdirAll(outDir, 0700)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Printf("Installing standalone Spotify client dir: %s\n", outDir)
+	resp, err := http.Get(DIRECT_DOWNLOAD_URL)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	defer resp.Body.Close()
+
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	err = os.WriteFile(path.Join(outDir, "SpotifyFullSetup.exe"), content, 0700)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	cmd := exec.Command(path.Join(outDir, "SpotifyFullSetup.exe"), "/extract", outDir)
+	err = cmd.Run()
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	// os.MkdirAll(path.Join(outDir, "data", "AppData", "roaming"), 0700)
+	// os.MkdirAll(path.Join(outDir, "data", "AppData", "local"), 0700)
+
+	cmd = exec.Command(path.Join(outDir, "Spotify.exe"))
+	// cmd.Env = append(cmd.Env, fmt.Sprintf("APPDATA=%s", path.Join(outDir, "data", "AppData", "roaming")))
+	// cmd.Env = append(cmd.Env, fmt.Sprintf("USERPROFILE=%s", path.Join(outDir, "data")))
+	// cmd.Env = append(cmd.Env, fmt.Sprintf("LOCALAPPDATA=%s", path.Join(outDir, "data", "AppData", "local")))
+	err = cmd.Run()
+	if err != nil {
+		log.Panicln(err)
+	}
+}
