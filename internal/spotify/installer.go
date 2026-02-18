e0dad205069f0c656f943559f4ff4af25764c5ea
Jeffrey Reij <contact@jreij.info>
2026-02-18 06:52:49 +0100

    Update installer.go

--- internal/spotify/installer.go
+++ internal/spotify/installer.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"path"
 )
 
@@ -45,21 +44,16 @@ func InstallStandaloneSpotify(outDir string, overwriteExisting bool) {
 		log.Panicln(err)
 	}
 
-	cmd := exec.Command(path.Join(outDir, "SpotifyFullSetup.exe"), "/extract", outDir)
-	err = cmd.Run()
-	if err != nil {
-		log.Panicln(err)
-	}
-
+	ExtractSpotify(outDir)
 	// os.MkdirAll(path.Join(outDir, "data", "AppData", "roaming"), 0700)
 	// os.MkdirAll(path.Join(outDir, "data", "AppData", "local"), 0700)
 
-	cmd = exec.Command(path.Join(outDir, "Spotify.exe"))
+	// cmd = exec.Command(path.Join(outDir, "Spotify.exe"))
 	// cmd.Env = append(cmd.Env, fmt.Sprintf("APPDATA=%s", path.Join(outDir, "data", "AppData", "roaming")))
 	// cmd.Env = append(cmd.Env, fmt.Sprintf("USERPROFILE=%s", path.Join(outDir, "data")))
 	// cmd.Env = append(cmd.Env, fmt.Sprintf("LOCALAPPDATA=%s", path.Join(outDir, "data", "AppData", "local")))
-	err = cmd.Run()
-	if err != nil {
-		log.Panicln(err)
-	}
+	// err = cmd.Run()
+	// if err != nil {
+	// log.Panicln(err)
+	// }
 }
