0fde28ea2ea9ed21775c69cccbcd60a7eebe71a7
Jeffrey Reij <contact@jreij.info>
2026-02-18 17:12:05 +0100

    Update patcher.go

--- internal/patcher/patcher.go
+++ internal/patcher/patcher.go
@@ -2,9 +2,20 @@ package patcher
 
 import (
 	"log"
+	"os"
+	"patcher/config"
+	"patcher/patches"
+	"path"
 )
 
-func PatchSpotifyClient(clientDir string) {
+func PatchSpotifyClient(clientDir string, config *config.Config) {
 	log.Printf("Patching spotify client located at: %s\n", clientDir)
 	ExtractSPAApps(clientDir)
+	if err := patches.PatchDevTools(path.Join(os.Getenv("LOCALAPPDATA"), "Spotify", "offline.bnk"), config.EnableDeveloperTools); err != nil {
+		log.Printf("Could not toggle devtools, %v\n", err)
+	}
+
+	if err := patches.PatchV8ContextSnapshot(path.Join(clientDir, "v8_context_snapshot.bin"), path.Join(clientDir, "Apps", "xpui")); err != nil {
+		log.Panicln(err)
+	}
 }
