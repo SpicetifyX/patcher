63f3a0c073b09aebdca4232c6112c073fb1ca4d0
Jeffrey Reij <contact@jreij.info>
2026-02-18 06:52:37 +0100

    Create extract.go

---
+++ internal/spotify/extract.go
@@ -0,0 +1,46 @@
+package spotify
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path"
+)
+
+func ExtractSpotify(outDir string) {
+	if _, err := os.Stat(path.Join(outDir, "Apps")); err == nil {
+
+		CleanOutDir(outDir)
+	}
+
+	cmd := exec.Command(path.Join(outDir, "SpotifyFullSetup.exe"), "/extract", outDir)
+	err := cmd.Run()
+	if err != nil {
+		log.Panicln(err)
+	}
+
+}
+
+func CleanOutDir(outDir string) error {
+	entries, err := os.ReadDir(outDir)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		name := entry.Name()
+
+		if name == "SpotifyFullSetup.exe" {
+			continue
+		}
+
+		fullPath := path.Join(outDir, name)
+
+		err := os.RemoveAll(fullPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
